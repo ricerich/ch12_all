4f042be08ec6fbbf4c7699c1fc755999cee402ab
Joseph Yoon <ricerich@gmail.com>
2022-03-30 11:50:58 +0900

    12장 완료

--- app/src/main/java/com/example/ch12_all/MainActivity.kt
+++ app/src/main/java/com/example/ch12_all/MainActivity.kt
@@ -1,15 +1,19 @@
 package com.example.ch12_all
 
 import android.content.Context
+import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteOpenHelper
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity()
 {
+    lateinit var myHelper1 : MyDBHelper
+    lateinit var sqlDB : SQLiteDatabase
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,6 +28,8 @@ class MainActivity : AppCompatActivity()
         var btnInit = findViewById<Button>(R.id.btnInit)
         var btnInsert = findViewById<Button>(R.id.btnInsert)
         var btnSelect = findViewById<Button>(R.id.btnSelect)
+        var btnUpdate = findViewById<Button>(R.id.btnUpdate)
+        var btnDelete = findViewById<Button>(R.id.btnDelete)
 
         //1.database 생성 -> SQLiteOpenHelper:[생성자로 만듦] -> 상속받는 클래스 만들고(MyDBHelper) -> 객체 만들고
         //2.Table    생성 -> SQLiteOpenHelper:[onCreate()]
@@ -31,7 +37,82 @@ class MainActivity : AppCompatActivity()
         //4.Select 기능 만들기 -> SQLiteDatabase -> rawQuery() 메소드에 select 쿼리문 작성 -> Cursor에서 반환값처리
 
         //1.MyDBHelper클래스 객체 생성만 해도, 1.DB와 2.테이블이 만들어짐
-        var myHelper1 = MyDBHelper(applicationContext)
+        myHelper1 = MyDBHelper(this)
+//        btnSelect.callOnClick() //여기는 안먹힘! 밑에 코드가 아직 초기화 안되었기 때문에
+//
+        //초기화 버튼
+        btnInit.setOnClickListener {
+            sqlDB  = myHelper1.writableDatabase
+            myHelper1.onUpgrade(sqlDB,1,2)
+            sqlDB.close()
+        }
+
+        //입력 insert
+        btnInsert.setOnClickListener {
+            sqlDB  = myHelper1.writableDatabase
+            sqlDB.execSQL("insert into groupTBL values ('"+ edtName.text.toString() +"', "+edtNumber.text.toString()+");")
+            sqlDB.close()
+            Toast.makeText(applicationContext, "입력완료~!", Toast.LENGTH_SHORT).show()
+
+            btnSelect.callOnClick()
+        }
+
+        //수정
+        btnUpdate.setOnClickListener {
+            sqlDB  = myHelper1.writableDatabase
+//            var sql = "update groupTBL set gNumber= "+edtNumber.text.toString()+" where gName = '"+edtName.text.toString()+"'; "
+            if(edtName.text.toString() != "") {
+                var sql = " update "
+                sql += "         groupTBL "
+                sql += " set "
+                sql += "         gNumber= " + edtNumber.text.toString()
+                sql += " where "
+                sql += "         gName = '" + edtName.text.toString() + "'; "
+
+                sqlDB.execSQL(sql)
+                sqlDB.close()
+                Toast.makeText(applicationContext, "수정완료~!", Toast.LENGTH_SHORT).show()
+
+                btnSelect.callOnClick()
+            }
+        }
+
+        //삭제
+        btnDelete.setOnClickListener {
+            sqlDB  = myHelper1.writableDatabase
+            if(edtName.text.toString() != "")
+            {
+                sqlDB.execSQL("delete from groupTBL where gName = '"+edtName.text.toString()+"';")
+                sqlDB.close()
+                Toast.makeText(applicationContext, "삭제 완료~!", Toast.LENGTH_SHORT).show()
+
+                btnSelect.callOnClick()
+            }
+        }
+
+        btnSelect.setOnClickListener {
+            sqlDB  = myHelper1.readableDatabase
+
+//            sqlDB.execSQL("select gName, gNumber from groupTBL;")//이게 아님!!!
+            var c1 : Cursor = sqlDB.rawQuery("select gName, gNumber from groupTBL;", null)//이게 아님!!!
+
+            var gName : String =""
+            var gNumber : String =""
+
+            while(c1.moveToNext())
+            {
+                gName   = gName   + c1.getString(0) + "\r\n"
+                gNumber = gNumber + c1.getString(1) + "\r\n"
+            }
+
+            edtNameResult.setText(gName)
+            edtNumberResult.setText(gNumber)
+
+            c1.close()
+            sqlDB.close()
+        }
+
+        btnSelect.callOnClick() //이거 안먹힘!@ 왠지 모름~!
 
     }
     //내부 클래스로 만들겠다
